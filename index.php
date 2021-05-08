3052ba93eb74c3142cb1daf3c2f9b4d2ffffa2c9
vikas202022 <45347760+vikas202022@users.noreply.github.com>
2021-05-08 09:48:13 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div id="section1">
+        <div class="bg-container d-flex flex-column justify-content-end">
+            <div class="flat-card">
+                <div class="text-center">
+                    <h1 class="flat-home-main">Sunrise Avenue</h1>
+                    <p class="flat-home-content">Move to what moves you</p>
+                    <a class="btn btn-outline-primary" href="wanna-help.php"   role="button">Need Help</a>
+                    <a class="btn btn-outline-primary" href="statewise/index.php" role="button">Wanna Help</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  
+
+   
+
+    
+
+   
+    <script type="text/javascript" src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-static-website/js/ccbp-ui-kit.js"></script>
+<script src="index.js"></script>
+</body>
+
+</html>
